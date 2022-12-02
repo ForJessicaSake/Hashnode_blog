136f3f8f9099e8987062a26d098cc0339185fe52
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-12-02 21:53:01 +0100

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect,useState, useCallback } from "react";
 
 function App() {
+
+  const variables = { page: 0 };
+
+  const query = `
+  query GetUserArticles($page: Int!) {
+      user(username: "ForJessicasake") {
+          publication {
+              posts(page: $page) {
+                  title
+                  brief
+                  slug
+                  coverImage
+              }
+          }
+      }
+  }
+`;
+
+  const fetchData = async () => {
+    const data = await fetch("https://api.hashnode.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    });
+    const result = await data.json();
+    const post = result.data.user.publication.posts;
+    setPosts(post)
+  };
+
+  const memoedFunction = useCallback(()=>{
+    return fetchData()},[])
+
+  useEffect(() => {
+    memoedFunction()
+  });
+
+  const [posts, setPosts] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <h1 className="font-bold lg:text-4xl text-3xl text-center mt-10 mb-14">Hi, welcome to my blog</h1>
+      {posts &&
+        posts.map((post) => (
+          <article key={post.id} className="flex justify-between ml-6 mr-4 mb-6 items-center text-justify">
+            <img src={post.coverImage} alt="blog post cover" className="lg:w-96 w-36" />
+            <figcaption className="ml-6">
+            <h2 className="font-bold lg:text-lg text-sm mb-2">{post.title}</h2>
+            <p className="lg:text-base text-xs">{post.brief}</p>
+            </figcaption>
+          </article>
+        ))}
+    </section>
   );
 }
 
