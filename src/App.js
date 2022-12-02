19ac85ad1c81bad223243ba44b182b1c266978fc
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-12-02 22:02:31 +0100

    Initial push

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
     const result = await data.json();
     const post = result.data.user.publication.posts;
     setPosts(post)
+    setLoading(false)
   };
 
   const memoedFunction = useCallback(()=>{
@@ -44,10 +45,15 @@ function App() {
   });
 
   const [posts, setPosts] = useState(null);
-
+  const [loading, setLoading] = useState(true)
   return (
+
     <section className="App">
+
       <h1 className="font-bold lg:text-4xl text-3xl text-center mt-10 mb-14">Hi, welcome to my blog</h1>
+      
+      {loading && <p className="ml-6">fetching blogs...</p> }
+
       {posts &&
         posts.map((post) => (
           <article key={post.id} className="flex justify-between ml-6 mr-4 mb-6 items-center text-justify">
